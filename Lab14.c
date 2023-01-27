9445eedaf394357b2a66550933662d8cedfc8b07
EndsCreed <jonjondm2001@gmail.com>
2023-01-27 19:17:03 -0330

    Lab 1.4 Completed

--- Lab14.c
+++ Lab14.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-
+    int sum(const int*, const int*), diff(const int*, const int*), mult(const int*, const int*), remain(const int*, const int*);
+    double divide(const int*, const int*);
+    char buff[80]; int a, b;
+    printf("Enter first integer: ");
+    fgets(buff, sizeof(buff), stdin);
+    a = atoi(buff);
+    printf("Enter second integer: ");
+    fgets(buff, sizeof(buff),stdin);
+    b = atoi(buff);
+    printf(
+            "Sum: %i\n"
+            "Difference: %i\n"
+            "Product: %i\n"
+            "Quotient: %f\n"
+            "Remainder: %i\n",
+            sum(&a, &b), diff(&a, &b), mult(&a, &b), divide(&a, &b), remain(&a, &b));
 
     return 0;
 }
@@ -18,18 +34,17 @@ int mult(const int *a, const int *b) {
     return *a * *b;
 }
 
-int div(const int *a, const int *b) {
+double divide(const int *a, const int *b) {
     if (*b == 0) {
-        printf("Cannot divide by ");
-        return 0;
+        printf("Division by 0 found. Quotient and Remainder will be -1\n");
+        return -1;
     }
-    return *a / *b;
+    return (double)*a / (double)*b;
 }
 
 int remain(const int *a, const int *b) {
     if (*b == 0) {
-        printf("Cannot divide by ");
-        return 0;
+        return -1;
     }
     return *a % *b;
 }
