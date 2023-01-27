bcc7cd3988c34237540074420cb206a4dd3b7dde
EndsCreed <jonjondm2001@gmail.com>
2023-01-27 14:53:08 -0330

    Lab 1.2 completed

--- Lab12.c
+++ Lab12.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    char buff[20]; int n;
+    char buff[20]; int n; char c; float f; // Declaring all variables
     printf("Enter integer: ");
     fgets(buff, sizeof(buff), stdin);
+    n = atoi(buff); // Converting buffer to an integer
+    printf("Enter float: ");
+    fgets(buff, sizeof(buff), stdin);
+    f = atof(buff); // Converting buffer to a float
+    printf("Enter character: ");
+    fgets(buff, sizeof(buff), stdin);
+    c = buff[0]; // Getting first index of buffer for the character entered
+
+    printf("%c is a character\n %i is an integer\n %f is a real number", c, n, f);
   return 0;
 }
