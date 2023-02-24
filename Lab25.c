2029bf8ba62b8056f8bbd0e59ffe6d9bf9b7eac8
EndsCreed <jonjondm2001@gmail.com>
2023-02-24 11:36:37 -0330

    Lab 2.5 completed

---
+++ Lab25.c
@@ -0,0 +1,20 @@
+// Lab 2: Functions, Pointers, and Arrays.
+// Jon Mitchell & Tom Arklie
+
+#include <stdio.h>
+
+void shift(int *a, int *b, int *c) {
+    int temp = *c;
+    // ^ Temp variable to set a to, since c will be overwritten
+    *c = *b;
+    *b = *a;
+    *a = temp;
+}
+
+int main() {
+    int a = 10, b = 25, c = 60;
+
+    printf("a = %d\nb = %d\nc = %d\n\n", a, b, c);
+    shift(&a, &b, &c);
+    printf("a = %d\nb = %d\nc = %d", a, b, c);
+}
